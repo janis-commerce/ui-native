4c0ecc9e90312f5f3f50941e08c5dd5ab140aeb0
Fernando Colom <fernando_colom@fizzmod.com>
2022-05-27 09:57:16 -0300

    ci: :rocket: commits
    setup for commit lint, standar-version and husky

---
+++ commitlint.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	'type-enum': [
+		'feat',
+		'fix',
+		'ci',
+		'chore',
+		'docs',
+		'style',
+		'refactor',
+		'perf',
+		'test',
+	],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				'fix',
+				'ci',
+				'chore',
+				'docs',
+				'style',
+				'refactor',
+				'perf',
+				'test',
+			],
+		],
+	},
+};
