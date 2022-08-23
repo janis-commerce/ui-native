d7944dfa488588b3ff7ae90de2b5fede458c773e
Fernando Colom <fernando_colom@fizzmod.com>
2022-08-23 11:34:38 -0300

    test: fix test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,14 @@
 module.exports = {
-	env: {
-		es6: true,
-		jest: true,
-	},
-	extends: ['airbnb', 'prettier'],
-	globals: {
-		Atomics: 'readonly',
-		SharedArrayBuffer: 'readonly',
-		__DEV__: 'readonly',
-		fetch: false,
-	},
+	root: true,
+	extends: '@react-native-community',
 	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 2018,
-		sourceType: 'module',
-	},
-	plugins: ['react', 'prettier'],
+	plugins: ['@typescript-eslint'],
 	rules: {
-		'no-use-before-define': 'off',
-		'import/extensions': [
-			'error',
-			'ignorePackages',
-			{
-				js: 'never',
-				jsx: 'never',
-				ts: 'never',
-				tsx: 'never',
-			},
-		],
 		'prettier/prettier': 'error',
 		'react/jsx-filename-extension': [
 			'off',
 			{
-				extensions: ['.jsx', '.js', '.ts', '.tsx'],
+				extensions: ['.jsx', '.js', 'ts', 'tsx'],
 			},
 		],
 		'import/prefer-default-export': 'off',
@@ -44,14 +17,6 @@ module.exports = {
 		'react/static-property-placement': 'off',
 		'react/jsx-props-no-spreading': 'off',
 		'no-param-reassign': 'off',
-		'no-console': 'off',
-	},
-	settings: {
-		'import/resolver': {
-			'babel-module': {},
-			node: {
-				extensions: ['.js', '.jsx', '.ts', '.tsx'],
-			},
-		},
+		'no-console': 'error',
 	},
 };
