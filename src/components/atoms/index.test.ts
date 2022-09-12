4cb19d92c9db65b8ac06f8608c1ddf98e7b98d2b
Fernando Colom <fernando_colom@fizzmod.com>
2022-09-12 12:04:47 -0300

    test: fix jest test

---
+++ src/components/atoms/index.test.ts
@@ -0,0 +1,7 @@
+import Text from './index';
+
+describe('renders app correctly', () => {
+	it('as no errors occurs', () => {
+		expect(Text()).toBeTruthy();
+	});
+});
---
+++ src/components/atoms/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const index = () => {
+	return (
+		<View>
+			<Text>test</Text>
+		</View>
+	);
+};
+
+export default index;
