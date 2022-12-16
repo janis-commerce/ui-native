b6dd80dc83a0df0f7b269ce17dfd89073d57ec06
Fernando Colom <fernando_colom@fizzmod.com>
2022-12-16 16:43:26 -0300

    install and initial setup storybook

--- index.js
+++ index.js
@@ -3,5 +3,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import Storybook from './storybook';
 
-AppRegistry.registerComponent(appName, () => App);
+const Component = __DEV__ ? Storybook : App;
+AppRegistry.registerComponent(appName, () => Component);
