c03fdaf31dc438b2966a088178bc6b37ff6b18ea
Pablo Damian Berdun <pablodamianberdun@gmail.com>
2023-01-24 15:14:43 -0300

    added storybook

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -4,10 +4,11 @@
 // https://github.com/elderfo/react-native-storybook-loader.git
 
 function loadStories() {
+	require('./stories/Avatar/Avatar.stories');
 	require('./stories/Button/Button.stories');
 }
 
-const stories = ['./stories/Button/Button.stories'];
+const stories = ['./stories/Avatar/Avatar.stories', './stories/Button/Button.stories'];
 
 module.exports = {
 	loadStories,
