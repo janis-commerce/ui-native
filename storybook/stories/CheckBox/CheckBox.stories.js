a23b0f7175f7bfd1933036da938e691ffd1dcd58
Damian Morales <damian.morales@fizzmod.com>
2023-03-03 19:21:10 -0300

    add checkbox component, test & storybook

--- storybook/stories/CheckBox/CheckBox.stories.js
+++ storybook/stories/CheckBox/CheckBox.stories.js
@@ -3,16 +3,18 @@ import {storiesOf} from '@storybook/react-native';
 import {boolean, number, color} from '@storybook/addon-knobs';
 import CheckBox from '../../../src/components/CheckBox';
 import CenterView from '../CenterView';
+import {action} from '@storybook/addon-actions';
 
 storiesOf('CheckBox', module)
 	.addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
 	.add('default props', () => (
 		<CheckBox
-			value={boolean('value', {OFF: false}, false)}
+			value={boolean('value', true)}
+			onValueChange={action('onValueChange')}
 			customSize={number('customSize', 18)}
 			checkOnColor={color('checkOnColor', '#2979FF')}
 			checkOffColor={color('checkOffColor', '#939598')}
 			iconCheckColor={color('iconCheckColor', '#FFF')}
-			disabled={boolean('disabld', {OFF: false}, false)}
+			disabled={boolean('disabld', false)}
 		/>
 	));
