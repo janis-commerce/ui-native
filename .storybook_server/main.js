a73f67ea75e3614c6c5b2a517160a7613fa3d8ee
Pablo Nicolás Ortiz <pablonortiz05@hotmail.com>
2023-07-19 16:09:11 -0300

    Added react-native-server support

--- .storybook_server/main.js
+++ .storybook_server/main.js
@@ -1,5 +1,8 @@
 module.exports = {
 	stories: ['../storybook/stories/**/*.stories.@(js|jsx|ts|tsx)'],
 	env: () => ({}),
+	core: {
+		builder: 'webpack5',
+	},
 	addons: ['@storybook/addon-controls', '@storybook/addon-actions', '@storybook/addon-essentials'],
 };
