fe039a692aca2ad5de151ddcad2a74f9b3fff936
Fernando Colom <fernando_colom@janis.im>
2023-08-14 14:55:16 -0300

    add font roboton

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -6,4 +6,6 @@ export default create({
 	brandUrl: 'https://janis.im',
 	brandImage: 'https://app.janis.in/static/media/janis-logo-base.0cc15e53.svg',
 	brandTarget: 'https://janis.im',
+	fontBase: '"Roboto", sans-serif',
+	fontCode: 'Roboto',
 });
