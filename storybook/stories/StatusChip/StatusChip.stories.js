4e21ce21b22f47d3c202f41e2ab5068a06aea78f
Fernando Colom <fernando_colom@janis.im>
2023-08-14 12:35:53 -0300

    test deploy storybooks

--- storybook/stories/StatusChip/StatusChip.stories.js
+++ storybook/stories/StatusChip/StatusChip.stories.js
@@ -31,7 +31,7 @@ export const PassingItComponent = (props) => (
 			letterSpacing={0}
 			textDecorationLine={'none'}
 			textTransform={'none'}>
-			delivered
+			not delivered
 		</Text>
 	</StatusChip>
 );
