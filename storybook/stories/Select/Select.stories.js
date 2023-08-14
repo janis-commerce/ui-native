31043cfbdc4496a23ebbb3b3d5331d5695ec5d32
damian <damian.morales@fizzmod.com>
2023-08-14 17:26:44 -0300

    crear componente select

--- storybook/stories/Select/Select.stories.js
+++ storybook/stories/Select/Select.stories.js
@@ -6,22 +6,63 @@ export default {
 	title: 'Select',
 };
 
-const inlineStyles = {width: '100%', padding: 10};
-export const DefaultProps = (props) => (
-	<View style={inlineStyles}>
-		<Select {...props} />
-	</View>
-);
-
-DefaultProps.storyName = 'default props';
-
 const listaPaises = [
 	{label: 'Argentina', value: 'ar'},
 	{label: 'Chile', value: 'cl'},
 	{label: 'Mexico', value: 'mx'},
 ];
 
-DefaultProps.args = {
+const inlineStyles = {width: '100%', padding: 10};
+
+export const DisabledSelect = (props) => (
+	<View style={inlineStyles}>
+		<Select {...props} />
+	</View>
+);
+
+DisabledSelect.storyName = 'disabled';
+
+DisabledSelect.args = {
+	options: listaPaises,
+	label: 'Paises',
+	optionStyles: () => {},
+	multiOptionsText: 'others',
+	placeholder: 'seleccione un pais',
+	isDisabled: true,
+	isMulti: false,
+	isSearchable: false,
+	onSelectOption: (option) => option,
+};
+
+export const SingleProps = (props) => (
+	<View style={inlineStyles}>
+		<Select {...props} />
+	</View>
+);
+
+SingleProps.storyName = 'single select';
+
+SingleProps.args = {
+	options: listaPaises,
+	label: 'Paises',
+	optionStyles: () => {},
+	multiOptionsText: 'others',
+	placeholder: 'seleccione un pais',
+	isDisabled: false,
+	isMulti: false,
+	isSearchable: false,
+	onSelectOption: (option) => option,
+};
+
+export const MultiProps = (props) => (
+	<View style={inlineStyles}>
+		<Select {...props} />
+	</View>
+);
+
+MultiProps.storyName = 'multi select';
+
+MultiProps.args = {
 	options: listaPaises,
 	label: 'Paises',
 	optionStyles: () => {},
@@ -29,4 +70,26 @@ DefaultProps.args = {
 	placeholder: 'seleccione un pais',
 	isDisabled: false,
 	isMulti: true,
+	isSearchable: false,
+	onSelectOption: (option) => option,
+};
+
+export const SearcherProps = (props) => (
+	<View style={inlineStyles}>
+		<Select {...props} />
+	</View>
+);
+
+SearcherProps.storyName = 'with search';
+
+SearcherProps.args = {
+	options: listaPaises,
+	label: 'Paises',
+	optionStyles: () => {},
+	multiOptionsText: 'others',
+	placeholder: 'seleccione un pais',
+	isDisabled: false,
+	isMulti: false,
+	isSearchable: true,
+	onSelectOption: (option) => option,
 };
