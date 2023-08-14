e17c1f0e2f6a4f3d500ad1f16be5c12ab8e8db38
Fernando Colom <fernando_colom@janis.im>
2023-08-14 14:42:15 -0300

    test deploy storybooks

---
+++ .storybook/theme.js
@@ -0,0 +1,9 @@
+import {create} from '@storybook/theming';
+
+export default create({
+	base: 'light',
+	brandTitle: 'Janis UI apps',
+	brandUrl: 'https://janis.im',
+	brandImage: 'https://app.janis.in/static/media/janis-logo-base.0cc15e53.svg',
+	brandTarget: 'https://janis.im',
+});
