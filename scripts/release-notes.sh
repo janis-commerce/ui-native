1b860b0a191df8d57159e0324deaee24d9735839
Fernando Colom <fernando_colom@janis.im>
2023-08-23 17:29:03 -0300

    fix extension file in slack message file

--- scripts/release-notes.sh
+++ scripts/release-notes.sh
@@ -4,4 +4,4 @@ awk -v ver="$TAG" '
 	/^#+ \[/ { if (p) { exit }; if ($2 == "["ver"]") { p=1; next} } p
 	' CHANGELOG.md | sed '${/^[[:space:]]*$/d;}' > RELEASE_NOTES.md
 
-node ./scripts/send-slack-message.cjs
+node ./scripts/send-slack-message.js
