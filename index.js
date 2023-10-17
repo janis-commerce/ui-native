f10782f23608c0e948b59a923541fe877d84c3c4
Gonzalo Lopez <gonzalo.lopez@fizzmod.com>
2023-10-17 12:05:04 -0300

    index.js was fixed

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ import App from './App';
 import {name as appName} from './app.json';
 import Storybook from './storybook';
 
-const Component = App;
+const Component = __DEV__ ? Storybook : App;
 AppRegistry.registerComponent(appName, () => Component);
