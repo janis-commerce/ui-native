510f588ca42fbe873d0c14e6ea7949da65053653
Damian Morales <damian.morales@janis.im>
2024-05-31 14:03:45 -0300

    correcciones - crear componente button

--- App.tsx
+++ App.tsx
@@ -1,33 +1,9 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {View} from 'react-native';
-import Button from './src/components/Button';
+import {View, Text} from 'react-native';
 
 const App = () => (
-	<View style={{padding: 20, flexDirection: 'row'}}>
-		<Button
-			value="Box Container"
-			icon="box"
-			type={'main'}
-			variant={'outlined'}
-			color="warning"
-			isLoading={false}
-			// iconPosition={'right'}
-			disabled={false}
-			style={{flex: 1}}
-		/>
-		<Button
-			value="Button with Box Icon"
-			icon="box"
-			isLoading={false}
-			type={'main'}
-			variant={'outlined'}
-			color={'primary'}
-			// iconPosition="right"
-			disabled={false}
-			style={{flex: 1}}
-			// iconStyle={{color: 'red', fontSize: 30}}
-		/>
+	<View>
+		<Text>UI native</Text>
 	</View>
 );
 
