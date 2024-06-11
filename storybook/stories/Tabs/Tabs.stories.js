c46f55e65fc339548c8ba8f4810e35032f1b7a55
Damian Morales <damian.morales@janis.im>
2024-06-11 18:50:32 -0300

    creacion componente tabs

--- storybook/stories/Tabs/Tabs.stories.js
+++ storybook/stories/Tabs/Tabs.stories.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React, {useState} from 'react';
 import Tabs from '../../../src/components/Tabs';
 import {Text, View} from 'react-native';
