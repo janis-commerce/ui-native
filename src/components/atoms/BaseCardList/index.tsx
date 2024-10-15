ba2c6b3139373d43269a61ee23e02de48d485902
Pablo Nicolas Ortiz <pablonortiz05@hotmail.com>
2024-10-15 12:10:22 -0300

    Added shadow to component BaseCardList

---
+++ src/components/atoms/BaseCardList/index.tsx
@@ -0,0 +1,42 @@
+import React, {FC, ReactNode} from 'react';
+import {StyleSheet, View, ViewProps} from 'react-native';
+import {palette} from 'theme/palette';
+
+export interface BaseCardListProps extends ViewProps {
+	children: ReactNode;
+	isSelected?: boolean;
+}
+
+const BaseCardList: FC<BaseCardListProps> = ({children, isSelected = false, style, ...props}) => {
+	if (!children) {
+		return null;
+	}
+
+	const styles = StyleSheet.create({
+		container: {
+			backgroundColor: palette.base.white,
+			borderRadius: 20,
+			width: '100%',
+			padding: 16,
+			elevation: 5,
+		},
+		selectedContainer: {
+			borderWidth: 2,
+			borderColor: palette.primary.main,
+		},
+	});
+
+	const activeStyles = [
+		styles.container,
+		isSelected && styles.selectedContainer,
+		style && style,
+	].filter(Boolean);
+
+	return (
+		<View style={activeStyles} {...props}>
+			{children}
+		</View>
+	);
+};
+
+export default BaseCardList;
