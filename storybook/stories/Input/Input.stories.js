ac74ae137e5acd4a9072031abcb58fed696b18a9
christian97dd <christian.dediego@janis.im>
2024-12-05 14:57:09 -0300

    adjusted input story

--- storybook/stories/Input/Input.stories.js
+++ storybook/stories/Input/Input.stories.js
@@ -16,32 +16,10 @@ export default {
 
 const Template = (args) => <Input {...args} />;
 
-export const Currency = Template.bind({});
-Currency.args = {
-	placeholder: 'Enter amount',
-	type: 'currency',
-};
+export const DefaultProps = Template.bind({});
 
-export const Number = Template.bind({});
-Number.args = {
-	placeholder: 'Enter number',
-	type: 'number',
-};
+DefaultProps.storyName = 'With a placeholder example';
 
-export const Text = Template.bind({});
-Text.args = {
-	placeholder: 'Enter text',
-	type: 'text',
-};
-
-export const Email = Template.bind({});
-Email.args = {
-	placeholder: 'Enter email',
-	type: 'email',
-};
-
-export const Phone = Template.bind({});
-Phone.args = {
-	placeholder: 'Enter phone',
-	type: 'phone',
+DefaultProps.args = {
+	placeholder: 'Placeholder example',
 };
