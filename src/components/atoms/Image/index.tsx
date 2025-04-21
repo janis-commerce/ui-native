388895e2bb78143508321ad0e72ee0bc69b1f6e5
Gonzalo Lopez <gonzalo.lopez@fizzmod.com>
2025-04-21 15:45:48 -0300

    The image component has been updated

--- src/components/atoms/Image/index.tsx
+++ src/components/atoms/Image/index.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import {Image as ImageComp, ImageProps as ImageRNProps, ImageSourcePropType} from 'react-native';
+import React, {useState} from 'react';
+import Icon, {Props as NativeIconProps} from 'atoms/Icon';
+import {palette} from 'theme/palette';
+import {View, StyleSheet, ViewStyle} from 'react-native';
+import FastImage, {FastImageProps} from 'react-native-fast-image';
 
-interface ImageProps extends ImageRNProps {
-	source: ImageSourcePropType;
+interface ImageProps extends FastImageProps {
+	iconProps?: NativeIconProps;
+	iconBackgroundStyle?: ViewStyle;
 }
 
-const Image = ({source, ...props}: ImageProps) => {
-	if (!source || typeof source !== 'object' || !Object.keys(source).length) {
-		return null;
+const Image = ({source, iconProps, iconBackgroundStyle = {}, onError, ...props}: ImageProps) => {
+	const [showPlaceholderImage, setShowPlaceholderImage] = useState(false);
+	const styles = StyleSheet.create({
+		iconBackground: {
+			backgroundColor: palette.white.light,
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+		},
+	});
+
+	const placeholderStyle = [styles.iconBackground, iconBackgroundStyle, props.style].filter(
+		Boolean
+	);
+	const handleError = () => {
+		if (onError) {
+			onError();
+		}
+
+		setShowPlaceholderImage(true);
+	};
+
+	if (showPlaceholderImage || !source) {
+		return (
+			<View style={placeholderStyle}>
+				<Icon name="exclamation_circle" color={palette.white.dark} size={36} {...iconProps} />
+			</View>
+		);
 	}
 
-	const sourceKeys = Object.keys(source);
-	if (!sourceKeys.includes('uri')) {
-		return null;
-	}
-
-	return <ImageComp source={source} {...props} />;
+	return <FastImage onError={handleError} source={source} {...props} />;
 };
 
 export default Image;
